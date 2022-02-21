ec89d25bd7233fc2d421bdc7b7cbbfdad7b3ec0d
Mahammad07 <94453965+Mahammad07@users.noreply.github.com>
2022-02-21 03:40:07 -0600

    first

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function Prompt(){
+    
+    var x=document.getElementById('final').value;
+
+   if(x<18){
+       alert("boyu gelersen");
+       return;
+   }
+   else{
+       alert("sene olar")
+   }
+  
+
+}
